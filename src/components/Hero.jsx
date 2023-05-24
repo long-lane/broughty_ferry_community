f2e2458a8c7ad31ea9065cb48436de00ea93c54b
long-lane <tech@long-lane.co.uk>
2023-05-24 23:50:48 +0100

    started with hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,15 +13,18 @@ export const Hero = () => {
           we have opportunities for you to take part or volunteer within our
           projects and activities.
         </p>
-        <form className='subscription'
+        <form
+          className="subscription"
           onSubmit={(ev) => {
-            subscribe(ev, '123456');
+            subscribe(ev, "123456");
           }}>
           <input type="email" />
-          <input type="submit" value='Subscribe' />
+          <input type="submit" value="Subscribe" />
         </form>
       </article>
-      <article></article>
+      <article>
+        <img src="https://media.istockphoto.com/id/1472519495/photo/group-of-senior-friends-smiling-on-camera-after-yoga-lesson-at-city-park.jpg?s=1024x1024&w=is&k=20&c=VNVbkjtGxpm4N7EEnXnt9Q1nheoxORj6fppC02NIv5w=" />
+      </article>
     </section>
   );
 };
