2e9eb0c7898e5bfd67e0cdac22e1808a6263d599
long-lane <tech@long-lane.co.uk>
2023-05-25 10:18:20 +0100

    fiels for production

--- src/components/WhoWeAre.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const WhoWeAre = () => {
-  return (
-    <section>
-      <h2>who we are</h2>
-    </section>
-  );
-};
--- src/components/YouthGaff.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const YouthGaff = () => {
-  return (
-    <section>
-    <h3>youth gaff</h3>
-  </section>
-  )
-}
