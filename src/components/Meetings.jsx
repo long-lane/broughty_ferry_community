ec5e4dd7d5adf5665b4521f08962fc544838c201
long-lane <tech@long-lane.co.uk>
2023-05-28 12:40:11 +0100

    added meetings component

--- src/components/Meetings.jsx
+++ src/components/Meetings.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 
-export const Meetings = () => {
+export const Meetings = ({ meetings }) => {
   return (
     <section className="meetings">
-      <article id="meetings">
-        <h3>meetings</h3>
-      </article>
+      <h3>meetings</h3>
+      <p>What we've been chatting about</p>
+      <div>
+        {meetings.map((meeting) => {
+          return (
+            <article key={meeting.meetingDate}>
+              <img src={meeting.img} alt={meeting.alt} loading="lazy" />
+              <h4>{meeting.title}</h4>
+              <time dateTime={meeting.meetingDate}>
+                {" "}
+                {meeting.meetingDate}{" "}
+              </time>
+              <p>{meeting.text}</p>
+
+              {meeting.links ? (
+                <>
+                <h5>useful links</h5>
+                <ul>
+                  {meeting.links.map((link, index) => {
+                    return (
+                      <li key={`${meeting.meetingDate}-${index}`}>
+                        {console.log(`${meeting.meetingDate}-link-${index}`)}
+                        <a
+                          href={link.linkURL}
+                          target="__blank"
+                          rel="noopener noreferrer">
+                          {link.linkText}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+                </>
+              ) : (
+                ""
+              )}
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
