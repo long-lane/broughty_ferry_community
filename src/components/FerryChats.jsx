e00dab8899d93cd55c5634433ffa04e31c081066
long-lane <tech@long-lane.co.uk>
2023-05-29 21:50:43 +0100

    added scrolling to meetings tablet and mobiel

--- src/components/FerryChats.jsx
+++ src/components/FerryChats.jsx
@@ -2,44 +2,11 @@ import React from "react";
 import ferryChats from "../assets/img/ferry_chats.png";
 import { HashLink } from "react-router-hash-link";
 import { Meetings } from "./Meetings";
-import fraud from '../assets/img/meetings/fraud.webp'
 export const FerryChats = ({ props }) => {
   const { showBtn, showMap, showMeetings } = props;
-  const meetings = [
-    {
-      img: fraud,
-      alt: "alt text for image",
-      title: "Scams",
-      text: "On this session we found out about some really useful ways to report scams.  This info has been helpfully passed on by Angela who attended Ferry Chats last week.  At the session, we were surprised that few people knew these ways of reporting scams. Thanks Angela",
-      links: [
-        {
-          linkText: "Trading Standards in Scotland",
-          linkURL: "https://www.consumeradvice.scot",
-        },
-        {
-          linkText: "Citizens Advice Scotland",
-          linkURL: "https://www.cas.org.uk",
-        },
-        {
-          linkText: "The National Cyber Security Centre",
-          linkURL:
-            "https://www.ncsc.gov.uk/section/information-for/individuals-families",
-        },
-        {
-          linkText: "Friends Against Scams",
-          linkURL: "https://www.friendsagainstscams.org.uk/elearning/Dundee",
-        },
-        {
-          linkText: "Stop Scams: Call 159",
-          linkURL: "https://stopscamsuk.org.uk/159",
-        },
-      ],
-      meetingDate: "2023-05-16",
-    },
-  ];
   return (
     <>
-      {showMeetings ? <Meetings meetings={meetings} /> : ""}
+      {showMeetings ? <Meetings /> : ""}
       <section className="ferry-chats">
         <article>
           <img src={ferryChats} alt="" loading="lazy" />
--- src/components/Meetings.jsx
+++ src/components/Meetings.jsx
@@ -1,6 +1,102 @@
 import React from "react";
+import fraud from "../assets/img/meetings/fraud.webp";
 
-export const Meetings = ({ meetings }) => {
+export const Meetings = () => {
+
+  const meetings = [
+    {
+      img: fraud,
+      alt: "alt text for image",
+      title: "Scams",
+      text: "On this session we found out about some really useful ways to report scams.  This info has been helpfully passed on by Angela who attended Ferry Chats last week.  At the session, we were surprised that few people knew these ways of reporting scams. Thanks Angela",
+      links: [
+        {
+          linkText: "Trading Standards in Scotland",
+          linkURL: "https://www.consumeradvice.scot",
+        },
+        {
+          linkText: "Citizens Advice Scotland",
+          linkURL: "https://www.cas.org.uk",
+        },
+        {
+          linkText: "The National Cyber Security Centre",
+          linkURL:
+            "https://www.ncsc.gov.uk/section/information-for/individuals-families",
+        },
+        {
+          linkText: "Friends Against Scams",
+          linkURL: "https://www.friendsagainstscams.org.uk/elearning/Dundee",
+        },
+        {
+          linkText: "Stop Scams: Call 159",
+          linkURL: "https://stopscamsuk.org.uk/159",
+        },
+      ],
+      meetingDate: "2023-05-16",
+    },
+
+    {
+      img: fraud,
+      alt: "alt text for image",
+      title: "Scams",
+      text: "On this session we found out about some really useful ways to report scams.  This info has been helpfully passed on by Angela who attended Ferry Chats last week.  At the session, we were surprised that few people knew these ways of reporting scams. Thanks Angela",
+      links: [
+        {
+          linkText: "Trading Standards in Scotland",
+          linkURL: "https://www.consumeradvice.scot",
+        },
+        {
+          linkText: "Citizens Advice Scotland",
+          linkURL: "https://www.cas.org.uk",
+        },
+        {
+          linkText: "The National Cyber Security Centre",
+          linkURL:
+            "https://www.ncsc.gov.uk/section/information-for/individuals-families",
+        },
+        {
+          linkText: "Friends Against Scams",
+          linkURL: "https://www.friendsagainstscams.org.uk/elearning/Dundee",
+        },
+        {
+          linkText: "Stop Scams: Call 159",
+          linkURL: "https://stopscamsuk.org.uk/159",
+        },
+      ],
+      meetingDate: "2023-05-16",
+    },
+
+    {
+      img: fraud,
+      alt: "alt text for image",
+      title: "Scams",
+      text: "On this session we found out about some really useful ways to report scams.  This info has been helpfully passed on by Angela who attended Ferry Chats last week.  At the session, we were surprised that few people knew these ways of reporting scams. Thanks Angela",
+      links: [
+        {
+          linkText: "Trading Standards in Scotland",
+          linkURL: "https://www.consumeradvice.scot",
+        },
+        {
+          linkText: "Citizens Advice Scotland",
+          linkURL: "https://www.cas.org.uk",
+        },
+        {
+          linkText: "The National Cyber Security Centre",
+          linkURL:
+            "https://www.ncsc.gov.uk/section/information-for/individuals-families",
+        },
+        {
+          linkText: "Friends Against Scams",
+          linkURL: "https://www.friendsagainstscams.org.uk/elearning/Dundee",
+        },
+        {
+          linkText: "Stop Scams: Call 159",
+          linkURL: "https://stopscamsuk.org.uk/159",
+        },
+      ],
+      meetingDate: "2023-05-16",
+    },
+  ];
   return (
     <section className="meetings">
       <h3>meetings</h3>
@@ -24,7 +120,6 @@ export const Meetings = ({ meetings }) => {
                   {meeting.links.map((link, index) => {
                     return (
                       <li key={`${meeting.meetingDate}-${index}`}>
-                        {console.log(`${meeting.meetingDate}-link-${index}`)}
                         <a
                           href={link.linkURL}
                           target="__blank"
